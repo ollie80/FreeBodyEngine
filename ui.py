536fc04920b871511190764165fc56d48d44c049
ollie08 <hoverboy@outlook.com>
2025-04-19 17:49:30 +1000

    feat: UI Element Border Radius

--- ui.py
+++ ui.py
@@ -10,11 +10,72 @@ UIELEMENT_FRAG_SHADER = """
 in vec2 uv;
 out vec4 f_color;
 
+uniform vec2 resolution; 
+
+
+uniform vec4 borderRadius;
+uniform float borderWidth;
+uniform float borderColor;
+
+vec2 getAspect() {
+    if (resolution.y > resolution.x) {
+        return vec2(resolution.x / resolution.y, 1.0);
+    } 
+    return vec2(1.0, resolution.y / resolution.x);
+}
+
+float applyBorderRadius(vec4 radius, vec2 uvs) {
+    vec2 aspect = getAspect();
+    vec2 cornerDist;
+    float r = 0.0;
+    bool inCorner = false;
+
+   // Bottom-left
+    if (uvs.x < radius.w && uvs.y < radius.w * aspect.y) {
+        cornerDist = uvs;
+        r = radius.w;
+        inCorner = true;
+
+    // Bottom-right
+    } else if (uvs.x > 1.0 - radius.z && uvs.y < radius.z * aspect.y) {
+        cornerDist = vec2(1.0 - uvs.x, uvs.y);
+        r = radius.z;
+        inCorner = true;
+
+    // Top-left
+    } else if (uvs.x < radius.x && uvs.y > 1.0 - radius.x * aspect.y) {
+        cornerDist = vec2(uvs.x, 1.0 - uvs.y);
+        r = radius.x;
+        inCorner = true;
+
+    // Top-right
+    } else if (uvs.x > 1.0 - radius.y && uvs.y > 1.0 - radius.y * aspect.y) {
+        cornerDist = 1.0 - uvs;
+        r = radius.y;
+        inCorner = true;
+    }
+    if (inCorner) {
+        vec2 center = vec2(r);
+        vec2 offset = (cornerDist - center) / aspect;
+        float dist = length(offset);
+        if (dist > r) {
+            return 0.0;
+        }
+    }
+
+
+    return 1.0; // Inside or not in a corner
+}
+
 
 void main() {
-    
+    float alpha = 1.0;
 
-    f_color = vec4(uv, 1.0, 1.0);
+    alpha = applyBorderRadius(borderRadius, uv);
+    
+    // Distance from rounded corner's arc center
+    vec3 texColor = vec3(uv, 1.0);
+    f_color = vec4(texColor.rgb, alpha);
 }
 """
 
@@ -135,6 +196,29 @@ class UIElement:
         for uniform in self.program:
             if uniform == "rot":
                 self.program['rot'] = self.style.get("rotation", (0, 0, 0))
+            if uniform == "borderRadius":
+                self.program['borderRadius'] = self._get_border_radius()
+                print(self.program['borderRadius'].value)
+            if uniform == "resolution":
+                self.program['resolution'] = self.manager.scene.main.window_size
+
+    def _get_border_radius(self):
+        style = self.style.get("border-radius", 0)
+        if isinstance(style, tuple):
+            if len(style) == 2:
+                x = style[0]/100
+                y = style[1]/100
+                return (x, x, y, y)
+            elif len(style) == 4:
+                return (style[0]/100, style[1]/100, style[2]/100, style[3]/100)
+            
+            else:
+                ValueError("Incorect border radius format, must be (x, y) or (left, right, top, bottom)")
+        
+        elif isinstance(style, int) or isinstance(style, float):
+            x = style/100
+            return (x, x, x, x)
+    
 
     def _get_padding(self):
         style = self.style.get("padding", 0)
